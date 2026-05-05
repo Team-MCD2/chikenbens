d3561db4f40fdce74609f65504bd8efe899528cf
angoularaphael <germainraphaelangoulaonambele@gmail.com>
2026-05-05 13:18:06 +0200

    chore: suppression des scripts utilitaires temporaires

--- list_images.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('src/data/site.ts', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-matches = re.findall(r'"name":\s*"([^"]+)".*?"image":\s*"([^"]+)"', content, re.DOTALL)
-for name, img in matches:
-    short = img[:80] + '...' if len(img) > 80 else img
-    print(f'{name} => {short}')
